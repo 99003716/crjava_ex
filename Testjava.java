479e874bbbf2b4ff1f197b9aeb7fb7e7da9ad769
99003716 <78860334+99003716@users.noreply.github.com>
2021-03-25 17:15:52 +0530

    examples

---
+++ Testjava.java
@@ -0,0 +1,32 @@
+//Simple if statement in java
+/*class Test
+{
+public static void main(String args[])
+{
+int a = 16;
+if (a==10)
+System.out.println(a);
+
+else if (a==6)
+System.out.println(a);
+
+else
+System.out.println("Not Equal");
+}
+}
+
+*/
+//Loop statements
+
+class Test
+{
+public static void main(String args[])
+{
+int a=0;
+while(a<10)
+{
+System.out.println(a);
+a += 1;
+}
+}
+}
